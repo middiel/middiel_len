211f81b38a0c30f8605e19a12ae210b8c7c3954f
middiel <middiel_len@sina.com>
2017-10-23 15:40:09 +0800

    Revert "Update python.py"
    This reverts commit 5b67313cc10098f65c90a6d201bdec55c5dba21b.

--- python.py
+++ python.py
@@ -1,2 +1,13 @@
+<<<<<<< HEAD
 print("hello")
-
+<<<<<<< HEAD
+print("mengkl")
+案件覅昂剪刀剪
+将就爱你大惊
+进的覅改动能否将爱你
+=======
+print("mengkl")
+>>>>>>> parent of b313a53... 娴嬭瘯
+=======
+print("hello")
+>>>>>>> parent of b9343fe... 杈撳嚭
